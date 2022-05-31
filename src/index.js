086443f3c106ef0bee78496197bc9874bdeeda75
ViacheslavDedov <dedoff.v@gmail.com>
2022-05-31 14:34:28 +0300

    fix dz11

--- src/index.js
+++ src/index.js
@@ -1,1 +1,5 @@
-import './sass/main.scss';
+import './css/styles.css';
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
+import { renderMarkup } from './render-list';
---
+++ src/render-list.js
@@ -0,0 +1,27 @@
+export function renderMarkup( hits,  container) {
+    const markup = hits.map(({ largeImageURL, webformatURL, tags, likes, views, comments, downloads }) => {
+   return  `<a class="post" href=${largeImageURL}>
+    <div class="photo-card">
+    <div class="thumb">
+    <img class="img-content" src=${webformatURL}  alt='${tags}' loading="lazy" width=300/>
+    <div class="info">
+      <p class="info-item">
+        <b>Likes: ${likes}</b>
+      </p>
+      <p class="info-item">
+        <b>Views: ${views}</b>
+      </p>
+      <p class="info-item">
+        <b>Comments: ${comments}</b>
+      </p>
+      <p class="info-item">
+        <b>Downloads: ${downloads}</b>
+      </p>
+    </div>
+    </div>
+  </div>
+  </a>
+          `
+      }).join('');
+      container.insertAdjacentHTML("beforeend", markup);
+  }
