b6344503051b91cbe8d35ab8e1a330de5ba10ff7
ViacheslavDedov <dedoff.v@gmail.com>
2022-06-03 15:16:40 +0300

    fixed renderlist

--- src/render-list.js
+++ src/render-list.js
@@ -2,25 +2,25 @@ export function renderMarkup(hits, container) {
     const markup = hits.map(({ largeImageURL, webformatURL, tags, likes, views, comments, downloads }) => {
    return  `<a class="post" href=${largeImageURL}>
     <div class="photo-card">
-    <div class="thumb">
-    <img class="img-content" src=${webformatURL}  alt='${tags}' loading="lazy" width=300/>
-    <div class="info">
-      <p class="info-item">
-        <b>Likes: ${likes}</b>
-      </p>
-      <p class="info-item">
-        <b>Views: ${views}</b>
-      </p>
-      <p class="info-item">
-        <b>Comments: ${comments}</b>
-      </p>
-      <p class="info-item">
-        <b>Downloads: ${downloads}</b>
-      </p>
+      <div class="thumb">
+        <img class="img-content" src=${webformatURL}  alt='${tags}' loading="lazy" width=300/>
+          <div class="info">
+            <p class="info-item">
+              <b>Likes: ${likes}</b>
+            </p>
+            <p class="info-item">
+              <b>Views: ${views}</b>
+            </p>
+            <p class="info-item">
+              <b>Comments: ${comments}</b>
+            </p>
+            <p class="info-item">
+              <b>Downloads: ${downloads}</b>
+            </p>
+          </div>
+      </div>
     </div>
-    </div>
-  </div>
-  </a>
+    </a>
           `
       }).join('');
       container.insertAdjacentHTML("beforeend", markup);
